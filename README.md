ded8d0a220ef9609224d4b6f82afe6c93766fa93
Amandip Dutta <amandip@berkeley.edu>
2025-04-06 16:16:52 -0700

    update

--- README.md
+++ README.md
@@ -1,15 +1,13 @@
 ### 1. Install Dependencies
+
 Install all required dependencies using the requirements.txt file:
+
 ```bash
 pip install -r requirements.txt
 ```
 
 ### 2. Run Files
+
 ```bash
 python read_data.py
 ```
-
-**Automation Schema:** https://drive.google.com/file/d/12k1hB6pn9TxUj6CgYFiFniKTfAK0xuuD/view?usp=sharing
-
-**Reference Google Spreadsheet:** https://docs.google.com/spreadsheets/d/1jsTdN1fkj_3brJ1H6P_AShoDnsqDdG_iJi6811fL9Jc/edit?pli=1&gid=0#gid=0
-
--- read_data.py
+++ read_data.py
@@ -6,47 +6,46 @@ import os
 
 
 def run():
-    sheets_url = 'https://docs.google.com/spreadsheets/d/1jsTdN1fkj_3brJ1H6P_AShoDnsqDdG_iJi6811fL9Jc/edit?gid=0#gid=0'
+
+    # When actually implementing this, we will likely store tutor_name and sheet_url in a databse table or dictionary.
+    sheets_url = 'https://docs.google.com/spreadsheets/d/1uPpm1K4f9fx97sO6cTrNqyhBorXxNp9zHgC52wcMKIs/edit?gid=0#gid=0'
+    tutor_name = "Aidan"
+
     convert_url_to_sheets(sheets_url)
-    tutor_object = read_csv_file()
+    # tutor_object = read_csv_file()
+    read_csv_file()
 
-    print("Tutor Name: " + tutor_object.get_name())
+    # print("Tutor Name: " + tutor_object.get_name())
 
-    print("Tutor Students: " + str(tutor_object.get_sessions()))
+    # print("Tutor Students: " + str(tutor_object.get_sessions()))
 
-    print("Total Hours: " + str(tutor_object.get_total_hours()))
+    # print("Total Hours: " + str(tutor_object.get_total_hours()))
 
-    print("Total Pay: " + str(tutor_object.get_total_pay()))
+    # print("Total Pay: " + str(tutor_object.get_total_pay()))
+
+# Find the columns for total hours and total pay
+
+
+
 
 
 def read_csv_file():
     df = pd.read_csv("downloaded_data.csv")
-    os.remove("downloaded_data.csv")  # Remove the file after reading it
+    cols = find_total_columns(df)
+    # os.remove("downloaded_data.csv")  # Remove the file after reading it
 
-    # Get tutor name and convert to title case
-    tutor_name = df.columns.tolist()[0].title()
-    total_hours = df.at[0, 'TOTAL HOURS']
-    print(total_hours)
-    total_pay = df.at[0, 'TOTAL PAY']
+# Finds the exact row and column of a string in a pandas datagframe.
+def find_string_in_dataframe(df, search_string):
+    matches = []
 
-    # Delete name, TOTAL HOURS, and TOTAL PAY columns as they are no longer needed
-    df = df.drop(df.columns[0], axis=1)
-    df = df.drop("TOTAL HOURS", axis=1)
-    df = df.drop("TOTAL PAY", axis=1)
-
-    # print(tutor_name, total_hours, total_pay) # DEBUG
-    tutor_object = Tutors(tutor_name, total_hours, total_pay)
-
-    for __, row in df.iterrows():
-        student_name = row['STUDENT']
-        date = row['DATE']
-        hours = row['HOURS']
-        paid = row['PAID']
-
-        tutor_object.add_session(student_name, date, hours, paid)
-
-    return tutor_object
-
+    for col in df.columns:
+        exact_matches = df[df[col].astype(str) == search_string]
+        
+        if not exact_matches.empty:
+            for row_idx in exact_matches.index:
+                matches.append((row_idx, col))
+    
+    return matches if matches else None
 
 def convert_url_to_sheets(sheets_url):
 
